613e397179cb6250a28b1212626d8925d880a0ee
joe kabucho <32999243+joekabucho@users.noreply.github.com>
2019-01-16 17:05:40 +0300

    Add files via upload

---
+++ BatteryPickup_Ray.js
@@ -0,0 +1,48 @@
+﻿#pragma strict
+
+private var torchScript : TorchController;
+
+private var guiShow : boolean = false;
+
+var battery : GameObject;
+
+var rayLength = 10;
+
+function Start()
+{
+	torchScript = GameObject.Find("Torch").GetComponent(TorchController);
+}
+
+function OnGUI()
+{
+	if(guiShow == true)
+	{
+		GUI.Box(Rect(Screen.width / 2, Screen.height / 2, 100, 25 ), "Pick up batteries!");
+	}
+}
+
+function Update() 
+{
+	Debug.Log(guiShow);
+	var hit : RaycastHit;
+	var fwd = transform.TransformDirection (Vector3.forward);
+    
+    if (Physics.Raycast (transform.position, fwd, hit, rayLength)) 
+    {
+        if(hit.collider.gameObject.tag == "Battery")
+		{
+			guiShow = true;
+			if(Input.GetKeyDown("e"))
+			{
+				torchScript.flashLight.light.intensity += 0.5;
+				Destroy(battery);
+				guiShow = false;
+			}
+		}
+    }
+    
+    else
+    {
+    	guiShow = false;
+    }
+}
---
+++ TorchController.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+var torchClick : AudioClip;
+
+var flashLight : Light;
+
+var torchWind : AudioClip;
+
+function Start()
+{
+	flashLight.light.enabled = false;
+	flashLight.light.intensity = 2;
+}
+
+function Update()
+{
+	if(flashLight.light.enabled == true)
+	{
+		flashLight.light.intensity -= 0.1 * Time.deltaTime / 5;
+		//Debug.Log(flashLight.light.intensity);
+	}
+	
+	if(Input.GetKeyDown("f"))
+	{
+		audio.PlayOneShot(torchClick);
+		
+		if(flashLight.light.enabled == false)
+		{
+			flashLight.light.enabled = true;
+		}
+		
+		else
+		{
+			flashLight.light.enabled = false;
+		}
+	}
+	
+	if(flashLight.light.enabled == true && Input.GetKeyDown("q"))
+	{
+	
+		flashLight.light.intensity += 0.05;
+		audio.PlayOneShot(torchWind);
+	}
+}
